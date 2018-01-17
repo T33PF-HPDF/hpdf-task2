74f40aaeee8302813999601aa53afd9765759577
shivam <shivamd20@gmail.com>
2018-01-18 01:54:16 +0530

    App.js mod

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from 'material-ui/Grid';
+import MenuAppBar from './pcomponents/MenuAppBar';
+
+import EnhancedTable from './pcomponents/EnhancedTable';
+import ListOfTables from './pcomponents/ListOfTables';
+
+var initialStore={
+  tables:[
+    {
+      table_id:0,
+      table_name:'first',
+      columnNames:['row_id','name','class'],
+      rows:[
+        {
+          row_id:0,
+          data:'first'
+        }
+      ]
+    }
+  ],
+  selectedTable:0
+}
 
 class App extends Component {
   render() {
+
+
+    let tableList,onAddTable,selectedTable=0;
+
+    
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      <React.Fragment>
+
+      <MenuAppBar/>
+
+      <Grid container spacing ={ 10 } >
+          <Grid item xs={12} sm={3}>
+          <ListOfTables/>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 9 }>
+        <EnhancedTable tableName={""} rows={[]} columns={[]} onAddRow={()=>{}} onDeleteRow={()=>{}}/>
+       
+        </Grid>
+        </Grid>
+  </React.Fragment>
     );
   }
 }
