800921d515e4f442bdc9c9a0e61edebcd78971a6
shivam <shivamd20@gmail.com>
2018-01-18 22:17:50 +0530

    pushing states up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,50 @@ import MenuAppBar from './pcomponents/MenuAppBar';
 import EnhancedTable from './pcomponents/EnhancedTable';
 import ListOfTables from './pcomponents/ListOfTables';
 
+const columnData = [
+];
+
+let counter = 0;
+function createData(name, calories, fat, carbs, protein) {
+    counter += 1;
+    return { id: counter, name, calories, fat, carbs, protein };
+}
+
+let data = [
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Donut', 452, 25.0, 51, 4.9),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Honeycomb', 408, 3.2, 87, 6.5),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Jelly Bean', 375, 0.0, 94, 0.0),
+  createData('KitKat', 518, 26.0, 65, 7.0),
+  createData('Lollipop', 392, 0.2, 98, 0.0),
+  createData('Marshmallow', 318, 0, 81, 2.0),
+  createData('Nougat', 360, 19.0, 9, 37.0),
+  createData('Oreo', 437, 18.0, 63, 4.0),
+]
+
+function createColumn(columnId, numeric, label, disablePadding) {
+
+  return { id: columnId, numeric: numeric, disablePadding: disablePadding, label: label || columnId }
+
+}
+
+columnData.push(createColumn("name", false, 'Dessert (100g serving)', true));
+columnData.push(createColumn("calories", true, 'Calories'));
+columnData.push(createColumn("fat", true, 'Fat (g)'));
+columnData.push(createColumn("carbs", true, 'Carbs (g)'));
+columnData.push(createColumn("protein", true, 'Protein (g)'));
+
+
+function onDeleteRows(rowIDs){
+
+
+
+}
+
 var initialStore={
   tables:[
     {
@@ -43,7 +87,7 @@ class App extends Component {
           <ListOfTables/>
         </Grid>
         <Grid item xs={ 12 } sm={ 9 }>
-        <EnhancedTable tableName={""} rows={[]} columns={[]} onAddRow={()=>{}} onDeleteRow={()=>{}}/>
+        <EnhancedTable tableName={"Nutrition"} rows={data} columnData={columnData} onAddRow={()=>{}} onDeleteRows={onDeleteRows}/>
        
         </Grid>
         </Grid>
